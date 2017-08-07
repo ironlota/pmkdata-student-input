11bb71ba09295ed1233fe9fe14b1fbeac68f5981
ironlota <raydreww@gmail.com>
2017-08-07 13:57:16 +0700

    first commit using purecss, pikaday, validate.js

--- src/js/datepicker.js
+++ src/js/datepicker.js
@@ -1,9 +1,6 @@
 import datepicker from 'pikaday';
-//import datepicker from 'tiny-date-picker';
 import 'pikaday/css/pikaday.css';
-//import 'tiny-date-picker/tiny-date-picker.min.css';
 
 export default (inputDOM) => {
   new datepicker({field: document.querySelector(inputDOM)});
-  //datepicker(document.querySelector(inputDOM));
 };
