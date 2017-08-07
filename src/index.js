dc2b25f71490a0983b387850a861cb250466a2d9
Jonathan Christopher <jonathan.christopher@outlook.com>
2017-08-07 17:04:09 +0700

    Form HTML and CSS without external dependencies except Google Fonts

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './css/style.css';
-import datepicker from './js/datepicker';
-import enumIterator from './js/enums';
-
-datepicker('#birth_date');
-enumIterator("gender");
-enumIterator("department");
