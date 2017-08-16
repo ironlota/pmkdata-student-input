8c9869ddd8c05d4b0009e493d1a63a855ca2021f
Jonathan Christopher <jonathan.christopher@outlook.com>
2017-08-16 16:45:56 +0700

    Corrected departments to follow API format, added no-js fallback

--- development/js/index.js
+++ development/js/index.js
@@ -24,7 +24,7 @@ s.addEventListener('submit', function (event) {
   event.preventDefault();
   m.innerHTML = '';
   var data = formToJson(studentForm);
-  var url = 'http://localhost:3000/api/students';
+  var url = '/api/students';
   var xhr = new XMLHttpRequest();
   xhr.open('POST', url, true);
   xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
@@ -32,10 +32,10 @@ s.addEventListener('submit', function (event) {
     var students = JSON.parse(xhr.responseText);
     if (xhr.readyState === 4 && xhr.status === 201) {
       m.setAttribute('class', 'success');
-      m.innerHTML = 'Success! <br />' + escapeHTML(students.name) + ' has been registered!';
+      m.innerHTML = 'Selamat datang, ' + escapeHTML(students.name) + '!';
     } else {
       m.setAttribute('class', 'error');
-      m.innerHTML = 'Error! <br /> Message: ' + students.message;
+      m.innerHTML = 'Data gagal disimpan :(<br />Keterangan: ' + students.message;
     }
   };
   xhr.send(JSON.stringify(data));
