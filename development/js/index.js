eb8883dc5c51fbd1c936e3871584912aa515da73
Jonathan Christopher <jonathan.christopher@outlook.com>
2017-08-16 17:11:43 +0700

    Added date validity check

--- development/js/index.js
+++ development/js/index.js
@@ -1,3 +1,23 @@
+function daysInMonth(m, y) { // m is 1-indexed: 1-12
+    switch (m) {
+        case 2 :
+            return (y % 4 == 0 && y % 100) || y % 400 == 0 ? 29 : 28;
+        case 9 : case 4 : case 6 : case 11 :
+            return 30;
+        default :
+            return 31
+    }
+}
+
+function isDateValid(dateStr) {
+    var dates = dateStr.split('-');
+    if (dates.length != 3) return false;
+    var y = dates[0];
+    var m = dates[1];
+    var d = dates[2];
+    return y >= 1990 && y <= 2089 && m >= 0 && m < 12 && d > 0 && d <= daysInMonth(m, y);
+}
+
 function formToJson (form) {
   var formData = form.elements;
   var result = {};
@@ -24,6 +44,10 @@ s.addEventListener('submit', function (event) {
   event.preventDefault();
   m.innerHTML = '';
   var data = formToJson(studentForm);
+  if (!isDateValid(data.birth_date)) {
+    m.innerHTML = 'Tanggal lahir harus merupakan tanggal yang benar dalam format YYYY-MM-DD (tahun-bulan-tanggal)';
+    return false;
+  }
   var url = '/api/students';
   var xhr = new XMLHttpRequest();
   xhr.open('POST', url, true);
